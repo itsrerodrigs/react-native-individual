2037456918e119b8b39bb802461e63ca7c25ae51
itsrerodrigs <itsrerodrigs@gmail.com>
2024-11-18 16:46:19 -0300

    add assets

--- projeto-individual/src/components/Index.jsx
+++ projeto-individual/src/components/Index.jsx
@@ -1,11 +1,28 @@
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, Text, Image, Touchable, TouchableOpacity } from "react-native"
 // https://usbrandcolors.com/spotify-colors/
-export const Home = () => {
+// https://developer.spotify.com/documentation/design 
+// https://www.figma.com/community/file/1139173521942616788
+
+const albumArt = require('../../assets/img/liniker-caju-tour.jpg');
+
+export const HomePlay = () => {
     return (
         <>
             <View style={styles.container}>
-                <Text>Hello</Text>
-                <View />
+                <View style={styles.header}>
+                    <Text style={styles.musicTitle}>Caju</Text>
+                    <Text style={styles.artistName}>Liniker</Text>
+                </View>
+
+                <Image source={albumArt}
+                    style={styles.albumArt} />
+                <View style={styles.controls}>
+                    <TouchableOpacity>
+
+                    </TouchableOpacity>
+                </View>
+
+
             </View>
         </>
     )
@@ -14,9 +31,27 @@ export const Home = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#1db954",
+        backgroundColor: "#121212",
         textDecorationColor: "#ccc",
-        justifyContent: "center",
-        alignItems: "center"
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddinngVertical: 50,
+    },
+    header: {
+        alignItems: "center",
+    },
+    musicTitle: {
+        color: "white",
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    artistName: {
+        color: "#ccc",
+        fontSize: 16,
+    },
+    albumArt: {
+        width: 300,
+        height: 300,
+        borderRadius: 60,
     }
 })
