c9f595ec04b4e0ed3004376315c23e86a8c24071
itsrerodrigs <itsrerodrigs@gmail.com>
2024-11-18 18:38:28 -0300

    versao com icones

--- projeto-individual/App.tsx
+++ projeto-individual/App.tsx
@@ -1,7 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { HomePlay } from './src/components/Index';
-import { Icons } from './src/components/Icons';
 
 
 export default function App() {
--- projeto-individual/src/components/Index.jsx
+++ projeto-individual/src/components/Index.jsx
@@ -1,25 +1,71 @@
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native"
-import { Icons } from "./Icons.jsx";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 // https://usbrandcolors.com/spotify-colors/
 // https://developer.spotify.com/documentation/design 
 // https://www.figma.com/community/file/1139173521942616788
-
 const albumArt = require('../../assets/img/liniker-caju-tour.jpg');
+
+const connectIcon = require('../../assets/icons/Connect.png');
+const nextIcon = require('../../assets/icons/Next.png');
+const pauseIcon = require('../../assets/icons/Pause.png');
+const previousIcon = require('../../assets/icons/Previous.png');
+const queueIcon = require('../../assets/icons/Queue.png');
+const repeatIcon = require('../../assets/icons/Repeat.png');
+const shuffleIcon = require('../../assets/icons/Shuffle.png');
+const shareIcon = require('../../assets/icons/Share.png');
+
+
 
 export const HomePlay = () => {
     return (
         <>
             <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.headerTitle}>Caju</Text>
+                </View>
                 <View style={styles.albumContainer}>
                     <Image source={albumArt}
                         style={styles.albumArt} />
                 </View>
-                <View style={styles.header}>
+                <View style={styles.playing}>
                     <Text style={styles.musicTitle}>Caju</Text>
                     <Text style={styles.artistName}>Liniker</Text>
                 </View>
-                <Icons />
+
+                <View style={styles.controlsContainer}>
+                    <TouchableOpacity>
+                        <Image source={shuffleIcon} style={styles.iconSmall} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={previousIcon} style={styles.iconMedium} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={pauseIcon} style={styles.iconLarge} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={nextIcon} style={styles.iconMedium} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={repeatIcon} style={styles.iconSmall} />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.progressBarContainer}>
+                    <View style={styles.progressBar} />
+                </View>
+
+                <View style={styles.secondContainer}>
+                    <TouchableOpacity>
+                        <Image source={connectIcon} style={styles.iconSmall} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={queueIcon} style={styles.iconSmall} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={shareIcon} style={styles.iconSmall} />
+                    </TouchableOpacity>
+                </View>
+
             </View>
         </>
     )
@@ -31,6 +77,14 @@ const styles = StyleSheet.create({
         backgroundColor: "#121212",
     },
     header: {
+        alignItems: 'center',
+    },
+    headerTitle: {
+        textTransform: 'uppercase',
+        fontSize: 24,
+        color: 'white',
+    },
+    playing: {
         alignItems: 'flex-start',
         justifyContent: 'flex-start',
         marginLeft: 20,
@@ -41,12 +95,12 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: "bold",
         textTransform: 'uppercase',
-        textAlign: 'right',
+        textAlign: 'left',
     },
     artistName: {
         color: "#ccc",
         fontSize: 16,
-        textAlign: 'right',
+        textAlign: 'left',
     },
     albumContainer: {
         flex: 3,
@@ -60,5 +114,41 @@ const styles = StyleSheet.create({
         resizeMode: 'cover',
         borderRadius: 10,
         marginBottom: 180,
+    },
+    controlsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flex: 0.1,
+        alignSelf: 'stretch',
+        marginTop: 20,
+    },
+    secondContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flex: 0.08,
+        alignSelf: 'stretch',
+        justifyContent: 'space-between',
+        marginBottom: '5%',
+    },
+    progressBar: {
+        flex: 1,
+        backgroundColor: 'white',
+        width: '30%',
+    },
+    iconSmall: {
+        aspectRatio: 1,
+        height: '60%',
+        resizeMode: 'contain',
+    },
+    iconMedium: {
+        aspectRatio: 1,
+        height: '70%',
+        resizeMode: 'contain',
+    },
+    iconLarge: {
+        aspectRatio: 1,
+        height: '90%',
+        resizeMode: 'contain',
     },
 });
